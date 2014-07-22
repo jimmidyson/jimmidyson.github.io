68bd108d732812ce987dfcc4b0ad318f57bab085
Jimmi Dyson <jimmi.dyson@specsavers.com>
2014-07-22 20:57:13 +0100

    rebuilding site Tue 22 Jul 20:57:13 BST 2014

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin-proxy source
+git push origin source
 git subtree push --prefix=public git@github.com:jimmidyson/jimmidyson.github.io.git master
